113a16e052f0d5c611a42cb446478370c01f5b38
Diwakar-Aryan <85320291+Diwakar-Aryan@users.noreply.github.com>
2022-08-05 04:20:54 +0530

    db url update

--- mongoose.js
+++ mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = () => {
   return mongoose.connect(
-    'mongodb+srv://admin:root@learning.rmbnn.mongodb.net/ShortUrl'
+    'Some URL Please use .env files'
   )
 }
 
